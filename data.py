41a9aa54d35b79fdef7d5d645b4891fffce58729
Kaden McKeen <mckeen.kaden.school@hotmail.com>
2022-04-16 21:04:37 -0400

    Bug fix.

--- data.py
+++ data.py
@@ -41,13 +41,15 @@ class Im2LatexDataset(Dataset):
             return img
         
         percent = math.sqrt(CUTOFF / (shape[1]*shape[2]))
-        new_shape = (3, int(shape[1]*percent), int(shape[2]*percent))
+        new_shape = (int(shape[1]*percent), int(shape[2]*percent))
         
         print("shape", shape)
         print("new_shape", new_shape)
         
         resize = T.Resize(new_shape)
-        return resize(img)
+        img = resize(img)
+        print("img.shape", img.shape)
+        return img
 
     def __getitem__(self, index):
         if self.args.augment:
