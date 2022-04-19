f5886e472358202e46f50f40c07cfc62c4378690
shahma13 <manav.shah@mail.utoronto.ca>
2022-04-19 16:11:36 -0400

    untested eval

--- evaluate.py
+++ evaluate.py
@@ -9,8 +9,10 @@ from tqdm import tqdm
 
 from data import Im2LatexDataset
 from build_vocab import Vocab, load_vocab
-from utils import collate_transformer_fn
-from model import LatexProducer, Im2LatexModel
+from model.model_transformer import Im2LatexModelTransformer
+from model.model import Im2LatexModel
+from utils import collate_fn, collate_transformer_fn
+from model.decoding_updated import LatexProducerUpdated
 from model.score import score_files
 
 
@@ -18,10 +20,12 @@ def main():
     parser = argparse.ArgumentParser(description="Im2Latex Evaluating Program")
     parser.add_argument('--model_path', required=True,
                         help='path of the evaluated model')
+    parser.add_argument('--model_type', required=True,
+                        help='type of model to evaluate')
 
     # model args
     parser.add_argument("--data_path", type=str,
-                        default="./sample_data/", help="The dataset's dir")
+                        default="./data/images/", help="The dataset's dir")
     parser.add_argument("--cuda", action='store_true',
                         default=True, help="Use cuda or not")
     parser.add_argument("--batch_size", type=int, default=32)
@@ -31,48 +35,59 @@ def main():
     parser.add_argument("--ref_path", type=str,
                         default="./results/ref.txt", help="The file to store reference")
     parser.add_argument("--max_len", type=int,
-                        default=64, help="Max step of decoding")
+                        default=150, help="Max step of decoding")
     parser.add_argument("--split", type=str,
-                        default="validate", help="The data split to decode")
+                        default="test", help="The data split to decode")
 
     args = parser.parse_args()
 
-    # 加载 模型
+    # load checkpoint
     checkpoint = torch.load(join(args.model_path))
     model_args = checkpoint['args']
 
-    # 读入词典,设置其他相关参数
+    # load vocab
     vocab = load_vocab(args.data_path)
     use_cuda = True if args.cuda and torch.cuda.is_available() else False
 
-    # 加载测试集
-    data_loader = DataLoader(
-        Im2LatexDataset(args.data_path, args.split, args.max_len),
-        batch_size=args.batch_size,
-        collate_fn=partial(collate_transformer_fn, vocab.sign2id),
-        pin_memory=True if use_cuda else False,
-        num_workers=4
-    )
+    # load dataloader
+    if args.model_type == 'transformer':
+        data_loader = DataLoader(
+            Im2LatexDataset(args.data_path, args.split, args.max_len),
+            batch_size=args.batch_size,
+            collate_fn=partial(collate_transformer_fn, vocab.sign2id),
+            pin_memory=True if use_cuda else False,
+            num_workers=4
+        )
+        model = Im2LatexModelTransformer(args)
+    else:
+        data_loader = DataLoader(
+            Im2LatexDataset(args.data_path, args.split, args.max_len),
+            batch_size=args.batch_size,
+            collate_fn=partial(collate_fn, vocab.sign2id),
+            pin_memory=True if use_cuda else False,
+            num_workers=4
+        )
+        model = Im2LatexModel(
+            len(vocab), model_args.emb_dim, model_args.dec_rnn_h,
+            add_pos_feat=model_args.add_position_features,
+            dropout=model_args.dropout
+        )
 
-    model = Im2LatexModel(
-        len(vocab), model_args.emb_dim, model_args.dec_rnn_h,
-        add_pos_feat=model_args.add_position_features,
-        dropout=model_args.dropout
-    )
     model.load_state_dict(checkpoint['model_state_dict'])
 
     result_file = open(args.result_path, 'w')
     ref_file = open(args.ref_path, 'w')
 
-    latex_producer = LatexProducer(
+    latex_producer = LatexProducerUpdated(
         model, vocab, max_len=args.max_len,
-        use_cuda=use_cuda, beam_size=args.beam_size)
+        use_cuda=use_cuda, beam_size=args.beam_size, )
 
     for imgs, (tgt4training, tgt4cal_loss) in tqdm(data_loader):
         try:
             reference = latex_producer._idx2formulas(tgt4cal_loss)
             results = latex_producer(imgs)
         except RuntimeError:
+            print('ERROR in evaluate')
             break
 
         result_file.write('\n'.join(results))
